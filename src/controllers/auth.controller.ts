8927a245df0fc393f065213d744db0336d124451
hzapata9 <hzapata9@gmail.com>
2024-12-19 01:38:00 -0300

    Add .env file

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,10 +3,22 @@ import { Request, Response, NextFunction } from "express";
 import { userService } from "../services/user.service";
 import { authService } from "../services/auth.service";
 import { nextTick } from "process";
+import { authLoginSchema } from "../schemas/auth.schemas";
+import { HttpError } from "../utils/httpError.util";
 
 const login = async(req: Request, res: Response, next: NextFunction) => {
     try {
         const { email, password } = req.body;
+
+        // usando Joi para validar datos
+        /*
+        const { error, value } = authLoginSchema.validate(req.body);
+        if(error) {
+            throw new HttpError(error.message, 400);
+        }
+        const { email, password } = value;
+        */
+
         const token = await authService.loginWithEmailAndPassword(email, password);
         console.log("TOKEN: " , token);
         //res.json({});
